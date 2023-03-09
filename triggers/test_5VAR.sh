b72524cfb382f6abf9d9dbd05a9c3361a083a134
Iliana Genkova <iliana.genkova@noaa.gov>
2023-03-09 20:29:11 +0000

    Add *.FIXGMI and *.W3 triggers

---
+++ triggers/test_5VAR.sh
@@ -0,0 +1,3 @@
+ jtyp=gfs cyc=00 PDY=20230308 desc=5VAR bash -l /u/iliana.genkova/bin/cycqsub /lfs/h2/emc/obsproc/noscrub/iliana.genkova/Trigs.KEEP/jglobal_dump.wc2.pbs.W3
+
+ with export TANK_002001 pointing at dev decom
