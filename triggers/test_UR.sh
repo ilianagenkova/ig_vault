c9f6fe794271d83cb8d20c964bb12091bc80ccc7
Iliana Genkova <iliana.genkova@noaa.gov>
2023-03-20 16:12:08 +0000

    *.UR triggers

---
+++ triggers/test_UR.sh
@@ -0,0 +1,3 @@
+ jtyp=gfs cyc=00 PDY=20230310 desc=UR bash -l /u/iliana.genkova/bin/cycqsub /lfs/h2/emc/obsproc/noscrub/iliana.genkova/Trigs.KEEP/jglobal_dump_post.wc2.pbs.UR
+ jtyp=gfs cyc=00 PDY=20230310 desc=UR bash -l /u/iliana.genkova/bin/cycqsub /lfs/h2/emc/obsproc/noscrub/iliana.genkova/Trigs.KEEP/jglobal_prep_post.wc2.pbs.UR
+
